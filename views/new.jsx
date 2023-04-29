5bad2d2ef7334f1555479840075bf49636ef6f98
Colin <colindcirian@gmail.com>
2023-04-29 13:08:11 -0700

    finished

--- views/new.jsx
+++ views/new.jsx
@@ -1,39 +1,44 @@
-const React = require('react')
-const Default = require('./layouts/default')
+const React = require("react");
+const Default = require("./layouts/default");
 
-function New () {
-    return (
-      <Default>
-        <h2>Add a new bread</h2>
-        <form action="/breads" method="POST">
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            required
-          />
-          <label htmlFor="image">Image</label>
-          <input
-            type="text"
-            name="image"
-            id="image"
-            pattern="https?://.*"
-            defaultImage
-            />
-          <label htmlFor="hasGluten">Has Gluten?</label>
-          <input
-            type="checkbox"
-            name="hasGluten"
-            id="hasGluten"
-            defaultChecked
-          />
-          <br />
-          <input type="submit"/>
-        </form>
+function New() {
+  return (
+    <Default>
+      <h2>Add a new bread</h2>
+      <form action="/breads" method="POST">
+        <label htmlFor="name">Name</label>
+        <input type="text" name="name" id="name" required />
+        <label htmlFor="image">Image</label>
+        <input
+          type="text"
+          name="image"
+          id="image"
+          pattern="https?://.*"
+          defaultImage
+        />
 
-      </Default>
-    )
+        <label htmlFor="baker">Baker</label>
+        <select name="baker" id="baker">
+          <option value="Rachel">Rachel</option>
+          <option value="Monica">Monica</option>
+          <option value="Joey">Joey</option>
+          <option value="Chandler">Chandler</option>
+          <option value="Ross">Ross</option>
+          <option value="Phoebe">Phoebe</option>
+        </select>
+
+        <label htmlFor="hasGluten">Has Gluten?</label>
+        <input type="checkbox" name="hasGluten" id="hasGluten" defaultChecked />
+        <br />
+        <input type="submit" />
+        <div className="backButton">
+          <a href="/breads">
+            <button>Go back to the index</button>
+          </a>
+        </div>
+      </form>
+    </Default>
+  );
 }
 
-module.exports = New
+module.exports = New;
--- views/show.jsx
+++ views/show.jsx
@@ -13,13 +13,14 @@ function Show({ bread, index }) {
         have gluten.
       </p>
       <img src={bread.image} alt={bread.name} />
+      <p>{bread.getBakedBy()}</p>
+      <a href={`/breads/${bread.id}/edit`}><button>Edit</button></a>
       <li>
         <a href="/breads">Go home</a>
       </li>
       <form action={`/breads/${bread.id}?_method=DELETE`} method="POST">
         <input type="submit" value="DELETE" />
       </form>
-      <a href={`/breads/${bread.id}/edit`}><button>Edit</button></a>
     </Default>
   );
 }
